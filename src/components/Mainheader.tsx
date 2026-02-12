d14f2394ef6774f5dea28c2a0b80ecbe09ad0d35
castrol-web <castrol.banda@students.jkuat.ac.ke>
2026-02-12 20:49:19 +0300

    Update Mainheader component with relevant content for Nyumbani Nala

--- src/components/Mainheader.tsx
+++ src/components/Mainheader.tsx
@@ -15,7 +15,7 @@ function Mainheader() {
         {
             heading: "Welcome to Nyumbani Nala ",
             text: [
-                "A Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.",
+                "In Tanzania, limited access to quality education and job opportunities remains one of the greatest challenges for vulnerable communities. At Nyumbani Nala Association, our core mission is to empower those most in need by opening pathways to education and entrepreneurship. Through close collaboration with local foundations and community leaders, we turn shared ideas into real, impactful projects that create lasting change.",
             ],
         },
         {
