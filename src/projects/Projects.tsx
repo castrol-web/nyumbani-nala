6f7e7affc032166aeb0c6cee87ff70ec59c184d0
castrol-web <castrol.banda@students.jkuat.ac.ke>
2026-04-05 13:39:19 +0300

    loading spinner

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -103,6 +103,7 @@ function Projects() {
     return (
       <div className="flex justify-center items-center py-20 innerset-0 mt-24">
         <div className="w-10 h-10 border-4 border-gray-300 border-t-[#F63049] rounded-full animate-spin mt-28"></div>
+        <span className="ml-4 text-gray-500">Loading projects...</span>
       </div>
     )
   }
